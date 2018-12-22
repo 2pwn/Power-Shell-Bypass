421bfb7859b1a35647fd6acf619e2f057aa8f7c1
2pwn <35379101+2pwn@users.noreply.github.com>
2018-12-22 16:12:25 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Linq;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
